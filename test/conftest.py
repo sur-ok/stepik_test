afee96544e28dbbfd12b150d56ca4c6d4e43a375
starikovVS <C0BECTb123?>
2023-10-30 21:22:00 +0300

    update test_items

--- test/conftest.py
+++ test/conftest.py
@@ -7,15 +7,12 @@ from selenium.webdriver.chrome.options import Options
 def browser(request):
     browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
-    browser = None
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     if browser_name == "chrome":
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         print("\nstart chrome browser for test..")
         browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
-        options = Options()
-        options.set_preference("intl.accept_languages", user_language)
         browser = webdriver.Firefox(options=options)
         print("\nstart firefox browser for test..")
     else:
@@ -28,5 +25,5 @@ def browser(request):
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default=None,
+    parser.addoption('--language', action='store', default="ru",
                      help="Choose language: ru, en")
--- test/test_items.py
+++ test/test_items.py
@@ -1,14 +1,10 @@
-import time
-
-import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
 class Test_items:
-    def test_visible_button(self, browser):
+    def test_clickable_button(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         browser.get(link)
-        time.sleep(10)
-        assert WebDriverWait(browser, 300).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#add_to_basket_form")))
+        assert WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#add_to_basket_form")))
